b062ab962d89bd5b715f2a3116c4c8a9a31e665a
hellokaton <hellokaton@gmail.com>
2023-10-24 18:00:39 +0800

    add cloudflare r2 example

--- src/main/java/com/example/tebis3api/AWSConfig.java
+++ src/main/java/com/example/tebis3api/AWSConfig.java
@@ -6,13 +6,15 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 
+@Data
 @Configuration
-@ConfigurationProperties(prefix = "tebi")
+@ConfigurationProperties(prefix = "r2")
 public class AWSConfig {
 
     private String accessKey;
@@ -42,54 +44,6 @@ public class AWSConfig {
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .enablePathStyleAccess()
                 .build();
-    }
-
-    public String getAccessKey() {
-        return accessKey;
-    }
-
-    public void setAccessKey(String accessKey) {
-        this.accessKey = accessKey;
-    }
-
-    public String getSecretKey() {
-        return secretKey;
-    }
-
-    public void setSecretKey(String secretKey) {
-        this.secretKey = secretKey;
-    }
-
-    public String getEndpointUrl() {
-        return endpointUrl;
-    }
-
-    public void setEndpointUrl(String endpointUrl) {
-        this.endpointUrl = endpointUrl;
-    }
-
-    public String getRegion() {
-        return region;
-    }
-
-    public void setRegion(String region) {
-        this.region = region;
-    }
-
-    public AWSCredentials getCredentials() {
-        return credentials;
-    }
-
-    public void setCredentials(AWSCredentials credentials) {
-        this.credentials = credentials;
-    }
-
-    public String getBucket() {
-        return bucket;
-    }
-
-    public void setBucket(String bucket) {
-        this.bucket = bucket;
     }
 
 }
